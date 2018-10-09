1674076b1c4c40e850b6a01bec19fa8290093e17
Scott Gigante <scottgigante@gmail.com>
2018-10-09 10:22:41 -0400

    use time.clock in python 2.7

--- tasklogger/__init__.py
+++ tasklogger/__init__.py
@@ -1,3 +1,10 @@
 from .logger import TaskLogger
 from .api import *
 from .version import __version__
+
+import time
+try:
+    time.process_time
+except AttributeError:
+    # Python 2.7 monkey patch
+    time.process_time = time.clock
