e48d761b49aafde22ec7ab26ec45c30cbb8341a4
Scott Gigante <scottgigante@users.noreply.github.com>
2022-07-05 10:13:11 -0400

    Add tests

--- test/test_stream.py
+++ test/test_stream.py
@@ -20,7 +20,30 @@ def test_stderr():
     sys.stdout = temp
 
 
-def test_invalid_stream():
+def test_invalid_stream_str():
     np.testing.assert_raises(
-        ValueError, tasklogger.TaskLogger, "invalid stream", stream="invalid"
+        ValueError, tasklogger.TaskLogger, 'Input stream is neither "stdout", "stderr", or a file-like', stream="invalid"
+    )
+
+def test_invalid_stream_str():
+    class InvalidStream:
+        pass
+    np.testing.assert_raises(
+        ValueError, tasklogger.TaskLogger, 'does not possess write() and flush() methods required of stream objects', stream=InvalidStream()
+    )
+
+def test_invalid_stream_noflush():
+    class InvalidStream:
+        def write(self):
+            pass
+    np.testing.assert_raises(
+        ValueError, tasklogger.TaskLogger, 'does not possess flush() method required of stream objects', stream=InvalidStream()
+    )
+
+def test_invalid_stream_nowrite():
+    class InvalidStream:
+        def flush(self):
+            pass
+    np.testing.assert_raises(
+        ValueError, tasklogger.TaskLogger, 'does not possess write() method required of stream objects', stream=InvalidStream()
     )
